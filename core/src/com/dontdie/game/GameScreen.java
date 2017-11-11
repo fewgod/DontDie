1e244f03019dfb57e5c014dd35488910fe18b958
Few <phuklagod@hotmail.com>
2017-11-11 20:59:14 +0700

    gain pos from player class& making move button

--- core/src/com/dontdie/game/GameScreen.java
+++ core/src/com/dontdie/game/GameScreen.java
@@ -1,23 +1,36 @@
 package com.dontdie.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	 
     private DontDieGame dontdieGame;
     private Texture playerImg;
- 
+    private Player1 player1;
+
     public GameScreen(DontDieGame dontdieGame) {
         this.dontdieGame = dontdieGame;
         playerImg = new Texture("Player1.png");
+        player1 = new Player1(100,100); //player starting pos
     }
     
     @Override
     public void render(float delta) {
+    	//update(delta);
+    	Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         SpriteBatch batch = dontdieGame.batch;
         batch.begin();
-        batch.draw(playerImg, 100, 100);
+        Vector2 player1Pos = player1.getPosition();
+        batch.draw(playerImg, player1Pos.x, player1Pos.y);
         batch.end();
     }
+
+	//private void update(float delta) {	
+	//}
 }
--- desktop/src/com/dontdie/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dontdie/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.dontdie.game.DontDieGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-        config.height = 600;
+		config.width = 900;
+        config.height = 700;
 		new LwjglApplication(new DontDieGame(), config);
 	}
 }
